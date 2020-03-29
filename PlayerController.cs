669b0978a9a0a3b5d98462d60d6990f7f4aed245
Brian Cain <brianccain@gmail.com>
2020-03-29 10:37:35 -0700

    Add initial files

---
+++ PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+  private float playerSpeed = 5f;
+  private Animator m_animator;
+
+  // Start is called before the first frame update
+  void Start() {
+    m_animator = GetComponent<Animator>();
+  }
+
+  // Update is called once per frame
+  void Update() {
+    MovePlayer();
+  }
+
+  private void MovePlayer() {
+    Vector2 move = GetMovementInput();
+    MovePlayerFree(move);
+  }
+
+  private Vector2 GetMovementInput() {
+    float horizontal = Input.GetAxisRaw("Horizontal");
+    float vertical   = Input.GetAxisRaw("Vertical");
+
+    // Lock directions to avoid diagonal movements
+    if (horizontal != 0f) {
+      vertical = 0f;
+    }
+
+    return new Vector2(horizontal, vertical);
+  }
+
+  // Free movement of player
+  private void MovePlayerFree(Vector2 moveVector) {
+    Vector3 position = transform.position;
+
+    UpdatePlayerAnimator(moveVector.x, moveVector.y);
+
+    if (moveVector.y > 0) {
+      position += Vector3.up;
+    }
+    if (moveVector.y < 0) {
+      position += Vector3.down;
+    }
+    if (moveVector.x > 0) {
+      position += Vector3.right;
+    }
+    if (moveVector.x < 0) {
+      position += Vector3.left;
+    }
+
+    transform.position = Vector3.MoveTowards(transform.position, position, Time.deltaTime*playerSpeed);
+  }
+
+  private void UpdatePlayerAnimator(float horizontal, float vertical) {
+    if (horizontal != 0f || vertical != 0f) {
+      m_animator.SetBool("Walking", true);
+      if (horizontal > 0f) {
+        m_animator.SetFloat("LastMoveHorizontal", 1f);
+      } else if (horizontal < 0f) {
+        m_animator.SetFloat("LastMoveHorizontal", -1f);
+      } else {
+        m_animator.SetFloat("LastMoveHorizontal", 0f);
+      }
+
+      if (vertical > 0f) {
+        m_animator.SetFloat("LastMoveVertical", 1f);
+      } else if (vertical < 0f) {
+        m_animator.SetFloat("LastMoveVertical", -1f);
+      } else {
+        m_animator.SetFloat("LastMoveVertical", 0f);
+      }
+    } else {
+      m_animator.SetBool("Walking", false);
+    }
+
+    m_animator.SetFloat("Horizontal", horizontal);
+    m_animator.SetFloat("Vertical", vertical);
+  }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Unity 2D Player Controller
+
+This simple script is a player controller for 2D games where the player moves on
+a horizontal and vertical axis. It includes some animation functions to be used
+with blend trees. It currently only keeps track of idle and walking states.
